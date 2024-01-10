bc92c8b0980bfc95812143433d20c7a438781310
bakaEC <1064071566@qq.com>
2024-01-10 11:41:40 +0800

    Initial commit

---
+++ src/views/invoice/Management.vue
@@ -0,0 +1,363 @@
+/**
+ * 基础菜单 商品管理
+ */
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索筛选 -->
+    <el-form :inline="true" :model="formInline" class="user-search">
+      <el-form-item label="搜索：">
+        <el-input size="small" v-model="formInline.deptName" placeholder="输入部门名称"></el-input>
+      </el-form-item>
+      <el-form-item label="">
+        <el-input size="small" v-model="formInline.deptNo" placeholder="输入部门代码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button size="small" type="primary" icon="el-icon-search" @click="search">搜索</el-button>
+        <el-button size="small" type="primary" icon="el-icon-plus" @click="handleEdit()">添加</el-button>
+      </el-form-item>
+    </el-form>
+    <el-upload class="upload-demo" action="http://127.0.0.1:8000" :on-success="handleSuccess" :before-upload="beforeUpload">
+    <el-button type="primary" icon="el-icon-upload2">点击上传ZIP文件</el-button>
+    </el-upload>
+    <el-table size="small" :data="listData" highlight-current-row v-loading="loading" border element-loading-text="拼命加载中" style="width: 100%;">
+      <el-table-column align="center" type="selection" width="60">
+      </el-table-column>
+      <el-table-column sortable prop="invoice_number" label="发票代码" width="300">
+      </el-table-column>
+      <el-table-column sortable prop="invoice_code" label="发票号码" width="300">
+      </el-table-column>
+      <el-table-column sortable prop="seller_name" label="售出方" width="300">
+      </el-table-column>
+      <el-table-column sortable prop="buyer_name" label="购买方" width="300">
+      </el-table-column>
+      <el-table-column sortable prop="invoice_date" label="发票日期" width="300">
+        <template slot-scope="scope">
+          <div>{{ scope.row.invoice_date }}</div>
+        </template>
+      </el-table-column>
+      <!-- 其他发票信息列 -->
+      <el-table-column align="center" label="操作" min-width="300">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="deleteUser(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页组件 -->
+    <Pagination v-bind:child-msg="pageparm" @callFather="callFather"></Pagination>
+    <!-- 编辑界面 -->
+
+
+  </div>
+</template>
+
+<script>
+import {deptDelete, deptSave} from '../../api/userMG'
+import Pagination from '../../components/Pagination'
+import axios from 'axios';
+
+
+export default {
+  data() {
+    return {
+      uploadedZipFile: null,
+      invoiceData: {
+        invoice_number: '',
+        invoice_code: '',
+        seller_name: '',
+        buyer_name: ''
+        // 添加其他发票信息字段
+      },
+
+      nshow: true, //switch开启
+      fshow: false, //switch关闭
+      loading: false, //是显示加载
+      editFormVisible: false, //控制编辑页面显示与隐藏
+      title: '添加',
+      editForm: {
+        deptId: '',
+        deptName: '',
+        deptNo: '',
+        token: localStorage.getItem('logintoken')
+      },
+      // rules表单验证
+      rules: {
+        deptName: [
+          { required: true, message: '请输入部门名称', trigger: 'blur' }
+        ],
+        deptNo: [{ required: true, message: '请输入部门代码', trigger: 'blur' }]
+      },
+      formInline: {
+        page: 1,
+        limit: 10,
+        varLable: '',
+        varName: '',
+        token: localStorage.getItem('logintoken')
+      },
+      // 删除部门
+      seletedata: {
+        ids: '',
+        token: localStorage.getItem('logintoken')
+      },
+      userparm: [], //搜索权限
+      listData: [], //用户数据
+      // 分页参数
+      pageparm: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 10
+      },
+
+    }
+  },
+
+  // 注册组件
+  components: {
+    Pagination
+  },
+  /**
+   * 数据发生改变
+   */
+
+  /**
+   * 创建完毕
+   */
+  created() {
+    this.getdata(this.formInline)
+  },
+
+  /**
+   * 里面的方法只有被调用才会执行
+   */
+  methods: {
+    // 在上传前验证文件
+    beforeUpload(file) {
+      const isZip = file.name.toLowerCase().endsWith('.zip');
+      if (!isZip) {
+        this.$message.error('请上传ZIP文件');
+      }
+      return isZip;
+    },
+    // 处理上传成功后的操作
+    handleSuccess(response, file) {
+      // if (response.success) {
+        this.$message.success('文件上传成功！');
+        // 将上传的文件传递给后端进行解析和入库
+        this.parseUploadedFile(file);
+      // } else {
+      //   this.$message.error(response.message || '文件上传失败！');
+      // }
+    },
+    // 调用后端解析文件的方法
+    parseUploadedFile(file) {
+      // 构建 FormData 对象，将文件作为参数发送给后端
+      const formData = new FormData();
+      formData.append('zipFile', file);
+
+      // 从Cookie中获取CSRF令牌的值
+      const csrfToken = this.getCookie('csrftoken');
+
+      // 添加CSRF令牌到请求头
+      const headers = {
+        'X_CSRFTOKEN': csrfToken,
+      };
+
+      // 发送请求给后端进行解析和入库
+      axios.post('http://127.0.0.1:8000/upload-invoice', formData, { headers })
+        .then(response => {
+          if (response.data.success) {
+            this.$message.success('文件解析成功！');
+            // 刷新界面或执行其他操作，显示解析结果等
+          } else {
+            this.$message.error(response.data.message || '文件解析失败！');
+          }
+        })
+        .catch(error => {
+          this.$message.error('文件解析失败，请稍后再试！');
+        });
+    },
+
+// 获取Cookie的函数
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+    },
+    // 获取公司列表
+    getdata(parameter) {
+      this.loading = true
+      // 模拟数据开始
+      let res = {
+        code: 0,
+        msg: null,
+        count: 5,
+        data: [
+          // {
+          //   addUser: null,
+          //   editUser: null,
+          //   addTime: 1521062371000,
+          //   editTime: 1526700200000,
+          //   deptId: 2,
+          //   deptName: 'XX分公司',
+          //   deptNo: '1',
+          //   parentId: 1
+          // },
+        ]
+      }
+      this.loading = false
+      this.listData = res.data
+      this.pageparm.currentPage = this.formInline.page
+      this.pageparm.pageSize = this.formInline.limit
+      this.pageparm.total = res.count
+
+      axios
+        .get('http://localhost:8000/api/invoices/', { params: parameter })
+        .then(response => {
+          this.loading = false;
+          this.listData = response.data.data;
+
+          // 提取第一张发票的信息，您可以根据需要更改索引
+          if (response.data.data.length > 0) {
+            const invoice = response.data.data[0];
+            this.invoiceData = {
+              invoice_number: invoice.invoice_number,
+              invoice_code: invoice.invoice_code,
+              seller_name: invoice.seller_name,
+              buyer_name: invoice.buyer_name
+              // 提取其他发票信息字段
+            };
+          }
+
+          // 分页赋值
+          this.pageparm.currentPage = parameter.page;
+          this.pageparm.pageSize = parameter.limit;
+          this.pageparm.total = response.data.count;
+        })
+        .catch(error => {
+          this.loading = false;
+          this.$message.error('数据加载失败，请稍后再试！');
+        });
+      // 模拟数据结束
+
+      /***
+       * 调用接口，注释上面模拟数据 取消下面注释
+       */
+
+    },
+
+
+
+    // 分页插件事件
+    callFather(parm) {
+      this.formInline.page = parm.currentPage
+      this.formInline.limit = parm.pageSize
+      this.getdata(this.formInline)
+    },
+    // 搜索事件
+    search() {
+      this.getdata(this.formInline)
+    },
+    //显示编辑界面
+    handleEdit: function(index, row) {
+      this.editFormVisible = true
+      if (row != undefined && row != 'undefined') {
+        this.title = '修改'
+        this.editForm.deptId = row.deptId
+        this.editForm.deptName = row.deptName
+        this.editForm.deptNo = row.deptNo
+      } else {
+        this.title = '添加'
+        this.editForm.deptId = ''
+        this.editForm.deptName = ''
+        this.editForm.deptNo = ''
+      }
+    },
+    // 编辑、增加页面保存方法
+    submitForm(editData) {
+      this.$refs[editData].validate(valid => {
+        if (valid) {
+          deptSave(this.editForm)
+            .then(res => {
+              this.editFormVisible = false
+              this.loading = false
+              if (res.success) {
+                this.getdata(this.formInline)
+                this.$message({
+                  type: 'success',
+                  message: '公司保存成功！'
+                })
+              } else {
+                this.$message({
+                  type: 'info',
+                  message: res.msg
+                })
+              }
+            })
+            .catch(err => {
+              this.editFormVisible = false
+              this.loading = false
+              this.$message.error('公司保存失败，请稍后再试！')
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    // 删除公司
+    deleteUser(index, row) {
+      this.$confirm('确定要删除吗?', '信息', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          deptDelete(row.deptId)
+            .then(res => {
+              if (res.success) {
+                this.$message({
+                  type: 'success',
+                  message: '公司已删除!'
+                })
+                this.getdata(this.formInline)
+              } else {
+                this.$message({
+                  type: 'info',
+                  message: res.msg
+                })
+              }
+            })
+            .catch(err => {
+              this.loading = false
+              this.$message.error('公司删除失败，请稍后再试！')
+            })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    },
+    // 关闭编辑、增加弹出框
+    closeDialog() {
+      this.editFormVisible = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-search {
+  margin-top: 20px;
+}
+.userRole {
+  width: 100%;
+}
+</style>
+
+
---
+++ src/views/invoice/invoiceInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>发票信息</h1>
+    <div v-html="invoiceData"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      invoiceData: ''
+    };
+  },
+  mounted() {
+    // 在组件挂载后，使用fetch或axios等方式获取数据
+    fetch('http://127.0.0.1:5000')
+      .then(response => response.text())
+      .then(data => {
+        // 将获取到的数据存储在invoiceData中，以便在模板中渲染
+        this.invoiceData = data;
+      })
+      .catch(error => {
+        console.error('发生错误:', error);
+      });
+  }
+};
+</script>
